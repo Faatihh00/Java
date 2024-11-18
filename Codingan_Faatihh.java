1ada907c990196cddbcabd83e9df1e3e814d513b
Faatihh00 <fatihjibril2007@gmail.com>
2024-11-18 09:43:38 +0700

    Add files via upload

---
+++ Codingan_Faatihh.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        String firstName = "Eren";
+         String lastName = "Jegerr";
+         System.out.println("nama pertama : "+firstName);
+         System.out.println("nama kedua : "+lastName);
+         System.out.println("panjang nama pertama : "+firstName.length());
+         System.out.println("panjang nama kedua : "+lastName.length());
+        
+    }
+}
